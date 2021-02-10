8cfe335984760a28a43eed33278d61c4d9b17cf1
Eric <eric.vollenweider@gmail.com>
2021-02-10 18:39:45 +0100

    Initial upload of Unity Project

---
+++ MrDrone.Unity/Assets/Drone/DroneManager.cs
@@ -0,0 +1,40 @@
+﻿using MrDrone.Core.Classes;
+using RosSharp.RosBridgeClient;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneManager : MonoBehaviour
+{
+    public RosConnector RosConnector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RosConnector.RosSocket.protocol.OnConnected += Protocol_OnConnected;
+    }
+
+    private void Protocol_OnConnected(object sender, System.EventArgs e)
+    {
+        InitializeDrone(RosConnector.RosSocket);
+    }
+
+    private void InitializeDrone(RosSocket socket)
+    {
+        Drone = new Drone(() => socket);
+    }
+
+    Drone Drone;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //The drone is not initialized yet. That could be because of the RosSocket not yet being connected to the ros-bridge
+        if (Drone == null) return;
+
+        //Update the drone's position
+        (var position, var rotation) = Drone.State.Pose.ToUnity();
+        this.transform.position = position;
+        this.transform.rotation = rotation;
+    }
+}
---
+++ MrDrone.Unity/Assets/MrDrone.Core.Extensions/IPoseExtensions.cs
@@ -0,0 +1,39 @@
+﻿using MrDrone.Core.Basics;
+using RosSharp;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class IPoseExtensions
+{
+    /// <summary>
+    /// Converts the pose into a Vector3 position and a Quaternion rotation in Unity's left handed coordinate system
+    /// </summary>
+    /// <param name="pose"></param>
+    /// <param name="convertRightToLeftHanded"></param>
+    /// <returns></returns>
+    public static (Vector3, Quaternion) ToUnity(this IPose pose, bool convertRightToLeftHanded = true)
+    {
+        Vector3 pos = new Vector3()
+        {
+            x = (float)pose.X,
+            y = (float)pose.Y,
+            z = (float)pose.Z
+        };
+        Quaternion rotation = new Quaternion()
+        {
+            x = (float)pose.i,
+            y = (float)pose.j,
+            z = (float)pose.k,
+            w = (float)pose.w,
+        };
+
+        if (convertRightToLeftHanded)
+        {
+            pos = TransformExtensions.Ros2Unity(pos);
+            rotation = TransformExtensions.Ros2Unity(rotation);
+        }
+
+        return (pos, rotation);
+    }
+}
